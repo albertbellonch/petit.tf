b0dfdb1932316bbe93b05f18c1df83b10bb91d0e
Albert Bellonch <albert@itnig.net>
2012-02-07 00:12:00 +0100

    Rails-ized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetitTk::Application.config.secret_token = '6fe2553de18875970d517533b87b590b40f6280ba5d57ddbcd47a1c4338fbf8eb21ddf94bfca4d0ce4e92b7669386993121c7006d1ae9764388857c3732d4458'
